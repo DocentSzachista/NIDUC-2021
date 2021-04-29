9473cf5a66cee2eb4a657098d562f15d93e47a6c
https://github.com/kuba342 <j.derda010@gmail.com>
2021-04-29 21:03:56 +0200

    Kontener

--- Container1.py
+++ Container1.py
@@ -9,13 +9,22 @@ class Container1:
          self.container.append(Packet1)
 
      def container_insert(self, Packet1, index):
-         self.container.insert(Packet1, index)
+         if index < len(self.container):
+             self.container.insert(Packet1, index)
+         else:
+             return None
 
      def container_pop(self):
-         return self.container.pop()
+         if len(self.container) > 0:
+             return self.container.pop()
+         else:
+             return None
 
      def container_get(self, index):
-         return self.container[index]
+         if index < len(self.container):
+            return self.container[index]
+         else:
+            return None
 
      def container_length(self):
          return len(self.container)
