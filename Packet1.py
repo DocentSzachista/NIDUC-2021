36e30e2cbb357f02e2467e3a91822ee24321c95b
https://github.com/kuba342 <j.derda010@gmail.com>
2021-05-08 11:24:18 +0200

    Packet1 - metody

--- Packet1.py
+++ Packet1.py
@@ -4,9 +4,18 @@ class Packet1:
         self.key = key
         """Pole przechowujące wartość do wysłania"""
         self.value = value
+        """Informacja o potrzebie retransmisji"""
+        self.retransmission = False
 
     def to_string(self):
         return f"Key: {self.key}, value: {self.value}"
+
+    def add_bit(self, bit):
+        if bit == 1 or bit == 0:
+            self.value.append(bit)
+
+    def clear_key(self):
+        self.key.clear()
 
     # Settery i gettery do enkapsulacji danych
     def get_key(self):
@@ -20,3 +29,10 @@ class Packet1:
 
     def set_value(self, value):
         self.value = value
+
+    def get_retransmission(self):
+        return self.retransmission
+
+    def set_retransmission(self, ret):
+        if ret == True or ret == False:
+            self.retransmission = ret
